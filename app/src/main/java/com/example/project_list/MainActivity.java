e43bf8f22cfd21c23deb9db207773ad0aff769c4
TDavidTouitou <touitou@et.esiea.fr>
2020-04-15 16:57:04 +0200

    TD3 fini

--- app/src/main/java/com/example/project_list/MainActivity.java
+++ app/src/main/java/com/example/project_list/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
 
@@ -22,19 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        // use this setting to
-        // improve performance if you know that changes
-        // in content do not change the layout size
-        // of the RecyclerView
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+
         List<String> input = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             input.add("Test" + i);
         }// define an adapter
-        mAdapter = new MyAdapter(input);
+        mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
 
     }
